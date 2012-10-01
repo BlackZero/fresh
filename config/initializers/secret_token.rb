f2680c6517298bd608a0ecbe3460efae951befa4
Ahmad Ayyash <ayyash@tanasuk.com>
2012-10-01 05:00:53 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrailsapp::Application.config.secret_token = '86d0f08259a64987f56b2a5bf170525ad597a97a8984728fde3e6b839981ae079a278b82c0613731385db05b4855711fdc7ef786857c1ea2371ecee11314eb60'
